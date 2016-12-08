905a80d21f75b890432ad95c152ebd32bfbe020c
Nick Yonts <NickYonts@Gmail.Com>
2016-12-07 18:50:44 -0600

    Added the header file for the Pipeline class

--- Pipeline.hpp
+++ Pipeline.hpp
@@ -2,17 +2,33 @@
 #define PIPELINE_HPP
 
 #include <gst/gst.h>
+#define GST_USE_UNSTABLE_API
+#include <gst/gl/gl.h>
+#include <gst/gl/gstglconfig.h>
+#include <gst/gl/egl/gstgldisplay_egl.h>
+#include <gst/video/video.h>
 
+#include <QOpenGLContext>
 
 class Pipeline : public QObject
 {
   Q_OBJECT
   
   public:
-    Pipeline();
-    
+    Pipeline(QOpenGLContext *);
+
+  private:
+    void CreatePipeline();
+
+    static void ClientDraw(GstElement *, GstGLContext *, GstSample *, Pipeline *);
+    static gboolean BusMessage(GstBus *, GstMessage *);
+    static gboolean SyncBusMessage(GstBus *, GstMessage *, Pipeline *);
+
   private:
     GstElement * mPipeline;
+    GstBus * mBus;
+    GstGLDisplay * mDisplay;
+    GstGLContext * mContext;
 };
 
 #endif
